c9186d62a96b5636091a0613e714d06515ee24da
Rabbi1146 <abdurrahmanrabbi51@gmail.com>
2025-04-11 11:38:43 +0600

    Delete project directory

--- project/bus_types.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO bus_types (id, type_name, capacity, model) VALUES
-(1, 'Common', 50, 'Non AC'),
-(2, 'Employee', 30, 'AC'),
-(3, 'Fixed', 45, '3 Star Series'),
-(4, 'Friday', 40, '5 Star Series');
